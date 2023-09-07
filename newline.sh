c1c127bacb8a0e7db34763631e5ad2dc0e477257
Quan Nguyen <128902312+quannhg@users.noreply.github.com>
2023-09-07 07:35:29 +0700

    fix(newline.sh): using echo with -e flag to insert newline

--- newline.sh
+++ newline.sh
@@ -6,9 +6,8 @@ file_name="README.md"
 # Define the text to be appended to the file
 new_text="Welcome to sample git-workflow project."
 
-# Use 'echo' to append the new text to the file
-echo "/n" > "$file_name"
-echo "$new_text" > "$file_name"
+# Append the new line to the file
+echo -e "\n$new_text" >> "$file_name"
 
 # Print a message to confirm the content has been appended
 echo "New content has been appended to '$file_name'."
