8ba5d649eed53e1b400155d5ee6362a43378865e
Quan Nguyen <128902312+quannhg@users.noreply.github.com>
2023-09-07 07:54:06 +0700

    feat(newline.sh): complete newline.sh

---
+++ newline.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Define the file name
+file_name="README.md"
+
+# Define the text to be appended to the file
+new_text="Welcome to sample git-workflow project."
+
+# Append the new line to the file
+echo -e "\n$new_text" > "$file_name"
+
+# Print a message to confirm the content has been appended
+echo "New content has been appended to '$file_name'."
+
